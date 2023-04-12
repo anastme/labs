d2e58f26e44df049511f80e23403d01e3758fc2d
Daniel <danielmorozov58@gmail.com>
2023-04-12 14:40:26 -0700

    lab1

---
+++ lab1/src/library/Driver.java
@@ -0,0 +1,12 @@
+package library;
+
+public class Driver {
+	
+	public String name;
+	public int experience;
+	
+	public Driver (String n, int e){
+		name = n;
+		experience = e;
+	}
+}
---
+++ lab1/src/library/Engine.java
@@ -0,0 +1,12 @@
+package library;
+
+public class Engine {
+
+	public String country;
+	public int power;
+	
+	public Engine (String c, int p){
+		country = c;
+		power = p;
+	}
+}
